bd9c9881efb7618a2afefd35699baee7d6a20bc9
CasellaLautaro <lautaromcasella@gmail.com>
2021-09-23 20:35:51 -0300

    Probando formateo de prettier

--- src/components/FormularioInicioSesion.js
+++ src/components/FormularioInicioSesion.js
@@ -12,52 +12,52 @@ import ContenedorBoton from "../elements/ContenedorBoton";
 import LineaSeparacion from "../elements/LineaSeparacion";
 
 const FormularioInicioSesion = () => {
-  const onSubmit = (e) => {};
+	const onSubmit = (e) => {};
 
-  return (
-    <>
-      <TituloBienvenida>
-        <Titulosh2 tituloh2Grande>¡Bienvenido/a!</Titulosh2>
-        <Titulosh2 tituloh2Chico>Iniciar sesión</Titulosh2>
-      </TituloBienvenida>
+	return (
+		<>
+			<TituloBienvenida>
+				<Titulosh2 tituloh2Grande>¡Bienvenido/a!</Titulosh2>
+				<Titulosh2 tituloh2Chico>Iniciar sesión</Titulosh2>
+			</TituloBienvenida>
 
-      <Formulario>
-        <form
-          action=""
-          onSubmit={onSubmit}
-          style={{ height: "100%", textAlign: "center" }}
-        >
-          <div>
-            {/*<div className="input-inicio-sesion-email">
+			<Formulario>
+				<form
+					action=""
+					onSubmit={onSubmit}
+					style={{ height: "100%", textAlign: "center" }}
+				>
+					<div>
+						{/*<div className="input-inicio-sesion-email">
 							<input type="text" placeholder="DNI" />
 						</div>
 						<div className="input-inicio-sesion-password">
 							<input type="password" placeholder="Contraseña" />
 						</div>*/}
-            <ContenedorInputs>
-              <Input type="text" placeholder="DNI" className="" />
-            </ContenedorInputs>
+						<ContenedorInputs>
+							<Input type="text" placeholder="DNI" className="" />
+						</ContenedorInputs>
 
-            <ContenedorInputs>
-              <Input type="password" placeholder="Contraseña" />
-            </ContenedorInputs>
-          </div>
+						<ContenedorInputs>
+							<Input type="password" placeholder="Contraseña" />
+						</ContenedorInputs>
+					</div>
 
-          <OlvidastePassword>
-            <OlvidastePasswordLink href="https://www.google.com.ar">
-              ¿Olvidaste tu contraseña?
-            </OlvidastePasswordLink>
-          </OlvidastePassword>
+					<OlvidastePassword>
+						<OlvidastePasswordLink href="https://www.google.com.ar">
+							¿Olvidaste tu contraseña?
+						</OlvidastePasswordLink>
+					</OlvidastePassword>
 
-          <LineaSeparacion></LineaSeparacion>
+					<LineaSeparacion></LineaSeparacion>
 
-          <ContenedorBoton>
-            <Boton type="submit">Ingresar</Boton>
-          </ContenedorBoton>
-        </form>
-      </Formulario>
-    </>
-  );
+					<ContenedorBoton>
+						<Boton type="submit">Ingresar</Boton>
+					</ContenedorBoton>
+				</form>
+			</Formulario>
+		</>
+	);
 };
 
 export default FormularioInicioSesion;
--- src/components/IniciarSesion.js
+++ src/components/IniciarSesion.js
@@ -8,26 +8,26 @@ import Titulosh1 from "../elements/Titulosh1";
 import Boton from "../elements/Boton";
 
 const IniciarSesion = () => {
-  const onClick = (e) => {
-    return <Registro />;
-  };
+	const onClick = (e) => {
+		return <Registro />;
+	};
 
-  return (
-    <TarjetaLogin>
-      <SideRegistro>
-        <Titulosh1 centrar blanco tituloGrande>
-          Trámites de Equivalencias
-        </Titulosh1>
-        <Boton border onClick={onClick}>
-          Registrarse
-        </Boton>
-      </SideRegistro>
+	return (
+		<TarjetaLogin>
+			<SideRegistro>
+				<Titulosh1 centrar blanco tituloGrande>
+					Trámites de Equivalencias
+				</Titulosh1>
+				<Boton border onClick={onClick}>
+					Registrarse
+				</Boton>
+			</SideRegistro>
 
-      <FormularioMain>
-        <FormularioInicioSesion />
-      </FormularioMain>
-    </TarjetaLogin>
-  );
+			<FormularioMain>
+				<FormularioInicioSesion />
+			</FormularioMain>
+		</TarjetaLogin>
+	);
 };
 
 export default IniciarSesion;
