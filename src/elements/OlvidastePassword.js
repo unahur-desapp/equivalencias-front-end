90c62bfa750ba5504d348fae9756f0eab87e03d5
CasellaLautaro <lautaromcasella@gmail.com>
2021-09-23 23:17:09 -0300

    Verificando indentacion

--- src/elements/OlvidastePassword.js
+++ src/elements/OlvidastePassword.js
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
 
 const OlvidastePassword = styled.div`
-  margin-top: 22px;
-  border-radius: 20px;
-  background: #fff;
-  text-align: center;
+    margin-top: 22px;
+    border-radius: 20px;
+    background: #fff;
+    text-align: center;
 `;
 
 export default OlvidastePassword;
