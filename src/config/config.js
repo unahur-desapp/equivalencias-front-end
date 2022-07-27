abc929a5ed8ae6b5446541e70eefc8246fab93e3
lmcasella <lautaromcasella@gmail.com>
2022-07-27 19:33:28 -0300

    s

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
 export const config = {
     // apiUrl: 'http://localhost:3001/api'
-    apiUrl: 'https://unahur-equivalencias-backend.herokuapp.com/api'
+    apiUrl: 'http://unahur-equivalencias-backend.herokuapp.com/api'
 };
