14867e728c91fe7d16e015d58db340bd67f214a6
Ezequiel David <ezeq.david98@gmail.com>
2023-06-20 13:38:40 -0300

    page crear carreras

--- src/components/PageCRUDCarreras.jsx
+++ src/components/PageCRUDCarreras.jsx
@@ -7,16 +7,59 @@ import {
     InputLabel,
     MenuItem,
     Select,
-    TextField
+    TextField,
+    Button
 } from '@mui/material';
+import {
+    StandardInput,
+    AutocompleteInput
+} from './atoms/Input/InputMUI';
 import { GridTop } from '../GridTop';
 import { Titulos } from './atoms/Title/Titulos';
-import SearchIcon from '@mui/icons-material/Search';
-import TablaUsuarios from './TablaUsuarios';
 import { HeaderSuperUsuario } from './HeaderSuperUsuario';
-import { getUsuarios } from '../services/usuario_service';
+import { getCarreras, createCarrera } from '../services/carrera_service';
 
 const PageCRUDCarreras = () => {
+    const [formValue, setformValue] = useState({
+        nombre_carrera: '',
+        nombre_instituto: ''
+    });
+
+    const [carreras, setCarreras] = useState([]);
+
+    useEffect(() => {
+        const fetchCarreras = async () => {
+            const carreras = await getCarreras();
+            setCarreras(carreras);
+        };
+        fetchCarreras();
+    }, []);
+
+    const handleChange = (event) => {
+        try {
+            setformValue((formValue) => ({
+                ...formValue,
+                [event.target.name]: event.target.value
+            }));
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let objCarrera = {
+            nombre_carrera: formValue.nombre_carrera,
+            nombre_instituto: formValue.nombre_instituto
+        };
+        console.log(objCarrera);
+        /*
+        createCarrera(objCarrera).then((rpta) => {
+            console.log(rpta);
+            setCarreras('');
+        }); */
+    };
+
     return (
         <Grid container>
             <Grid item container xs={12}>
@@ -40,13 +83,70 @@ const PageCRUDCarreras = () => {
                     alignItems="center"
                 >
                     <Grid item xs={12}>
-                        <Titulos>CRUD Carreras</Titulos>
+                        <Titulos component="h2" titulogrande>
+                            CRUD Carreras
+                        </Titulos>
                     </Grid>
                 </GridTop>
+
+            
+                <Grid
+                    item
+                    container
+                    direction="column"
+                    alignItems="flex-start"
+                    md={12}
+                    lg={5.8}
+                    sx={{
+                        marginTop: '6px'
+                    }}
+                >
+                    <StandardInput
+                        required
+                        name="nombre_carrera"
+                        size="small"
+                        label="Nombre Carrera"
+                        variant="outlined"
+                        value={formValue.nombre_carrera || ''}
+                        onChange={handleChange}
+                    />
+                </Grid>
+
+                <Grid
+                    item
+                    container
+                    direction="column"
+                    alignItems="flex-start"
+                    md={12}
+                    lg={5.8}
+                    sx={{
+                        marginTop: '6px'
+                    }}
+                >
+                    <StandardInput
+                        required
+                        name="nombre_instituto"
+                        size="small"
+                        label="Nombre Instituto"
+                        variant="outlined"
+                        value={formValue.nombre_instituto || ''}
+                        onChange={handleChange}
+                    />
+                </Grid>
+
+                <Button
+                    variant="contained"
+                    color="primary"
+                    sx={{
+                        marginTop: '6px'
+                    }}
+                    onClick={handleSubmit}
+                >
+                    Crear Carrera
+                </Button>
             </Grid>
         </Grid>
-
-        );
+    );
 };
 
-export default PageCRUDCarreras;
+export { PageCRUDCarreras };
--- src/services/carrera_service.js
+++ src/services/carrera_service.js
@@ -10,3 +10,12 @@ export async function getCarrera(id) {
     const apiResponse = await axios.get(`${config.apiUrl}/carreras/${id}`);
     return apiResponse.data;
 }
+
+export async function createCarrera(carrera) {
+    const apiResponse = await axios.post(`${config.apiUrl}/carreras`, carrera, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    return apiResponse.data;
+}
